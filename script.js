fca9e931587dbdaf13dbfa1f4286efd8427afa73
Esteban Palacio <estipalacio@gmail.com>
2021-08-09 10:39:03 -0400

    adding function to fetch quotes from api

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Testing');
+let apiQuotes = []
+
+const newQuote = () => {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote);
+}
+
+// Get Quotes from API
+const getQuotes = async () => {
+    const apiUrl = 'https://type.fit/api/quotes'
+
+    try {
+        const response = await fetch(apiUrl)
+        apiQuotes = await response.json()
+        newQuote()
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+
+// On Load
+getQuotes()
